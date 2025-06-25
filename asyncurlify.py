84f0b79cb475af8121d44c0d83d64581de5f35e2
Vladimir Zhirnov <22815985+vzhirnov@users.noreply.github.com>
2025-06-25 20:57:31 +0700

    Fix body handling and remove aiohttp import

--- asyncurlify.py
+++ asyncurlify.py
@@ -1,10 +1,13 @@
-import aiohttp
 import json
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from aiohttp import ClientResponse
 
 from shlex import quote
 
 
-def to_curl(request: aiohttp.ClientResponse, body=None, compressed=False, verify=True):
+def to_curl(request: 'ClientResponse', body=None, compressed=False, verify=True):
 
     parts = [
         ("curl", None),
@@ -14,10 +17,10 @@ def to_curl(request: aiohttp.ClientResponse, body=None, compressed=False, verify
     for k, v in sorted(request.request_info.headers.items()):
         parts += [("-H", "{0}: {1}".format(k, v))]
 
-    if body:
+    if body is not None:
         if isinstance(body, dict):
-            body = json.dumps(body).encode("utf-8")
-        if isinstance(body, bytes):
+            body = json.dumps(body)
+        elif isinstance(body, bytes):
             body = body.decode("utf-8")
         parts += [("-d", body)]
 
@@ -31,8 +34,8 @@ def to_curl(request: aiohttp.ClientResponse, body=None, compressed=False, verify
 
     flat_parts = []
     for k, v in parts:
-        if k:
+        if k is not None:
             flat_parts.append(quote(k))
-        if v:
+        if v is not None:
             flat_parts.append(quote(v))
     return " ".join(flat_parts)
